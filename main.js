9ced1fa711a481dbf527499eb067ca0688d8ad18
mernaawd <125801027+mernaawd@users.noreply.github.com>
2024-05-26 01:07:58 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+var text=document. getElementById("PAR");
+
+function ChangeFont(e){
+    text. style.fontFamily=e;
+}
+ function ChangeAlign(e){
+    text.style.textAlign=e;
+ }
+ function ChangeHeight(e){
+    text.style.lineHeight=e;
+ }
+function ChangeLSpace(e){
+    text.style.letterSpacing=e;
+}
+function ChangeIndent(e){
+    text.style.textIndent=e;
+}
+function ChangeTransform(e){
+    text.style.textTransform=e;
+}
+function ChangeDecorate(e){
+    text.style.textDecoration=e;
+}
+function ChangeBorder(e){
+   text.style.border=e;
+}
+function ChangeBorderColor(e){
+   text.style.borderColor=e;
+}
+
+
+
